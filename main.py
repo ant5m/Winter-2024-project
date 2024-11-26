d6422a3e3400d47b99cb592ef37756b7b2b77c2a
ant <anthonysevilla@crc-dot1x-nat-10-239-27-175.bu.edu>
2024-11-26 11:16:38 -0500

    added code that allows for user login

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from dotenv import load_dotenv
 import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-print(client_id, client_secret) 
+
+# Set up the authentication flow
+scope = "user-read-private user-read-email"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id,
+                                               client_secret,
+                                               redirect_uri="http://localhost",
+                                               scope='user-top-read'))
+
+
+user = sp.current_user()
+
+top_tracks = sp.current_user_top_tracks(limit=50, time_range='medium_term')
+
+for track in top_tracks['items']:
+    print(track['name'], 'by', track['artists'][0]['name'])
+    
+    
